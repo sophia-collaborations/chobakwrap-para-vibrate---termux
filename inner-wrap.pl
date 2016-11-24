36d8a29ab440afa6c9cd07cf810142314b122a76
Sophia Shapira <Sophia.Shapira@gmail.com>
2016-11-24 10:33:36 -0500

    Added the -nomin option

--- inner-wrap.pl
+++ inner-wrap.pl
@@ -3,6 +3,7 @@ use argola;
 
 my $vbtime = 1000;
 my $vbminim = 30;
+my $vbreqmin = 10;
 
 sub opto__msec_do {
   $vbtime = int(&argola::getrg() + 0.45);
@@ -12,8 +13,13 @@ sub opto__sec_do {
   $vbtime = int((&argola::getrg() * 1000) + 0.45);
 } &argola::setopt('-sec',\&opto__sec_do);
 
+sub opto__nomin_do {
+  $vbreqmin = 0;
+} &argola::setopt('-nomin',\&opto__nomin_do);
+
 &argola::runopts();
 
+if ( $vbreqmin < 5 ) { $vbminim = 0; }
 system("echo",("Vibration milliseconds set to " . $vbtime . " (minimum is " . $vbminim . ")"));
 
 if ( $vbtime < ( $vbminim - 0.5 ) ) { exit(0); }
